d4a6800c4ada3ba90de5fe4b8c061323699a3830
ykarmr <yuki.arimura0508@gmail.com>
2025-04-16 10:25:39 +0900

    pino ロガーを追加し、クッキーの情報をログ出力するように変更

--- src/util/action.ts
+++ src/util/action.ts
@@ -1,9 +1,13 @@
 "use server";
 import { cookies } from "next/headers";
+import pino from "pino";
+
+const logger = pino();
 
 export const action = async () => {
   try {
-    console.log((await cookies()).getAll());
+    const cookieStore = await cookies();
+    logger.info(cookieStore.getAll());
     return "ok";
   } catch {
     return "ng";
