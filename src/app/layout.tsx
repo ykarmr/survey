7519386047b0dc8198ed8b08890c649878fb73a4
ykarmr <yuki.arimura0508@gmail.com>
2025-04-16 13:25:28 +0900

    refactor: BigInteger.jsスクリプトをbeforeInteractive戦略で追加

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Script from "next/script";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -24,6 +25,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <Script
+        src="https://peterolson.github.io/BigInteger.js/BigInteger.min.js"
+        strategy="beforeInteractive"
+      />
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
