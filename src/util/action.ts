71c1ec1345e94178dcb64d0ee14a0a806a6ecd20
ykarmr <yuki.arimura0508@gmail.com>
2025-04-16 10:21:02 +0900

    cookies モジュールをインポートし、全クッキーをログ出力する機能を追加

--- src/util/action.ts
+++ src/util/action.ts
@@ -1,7 +1,9 @@
 "use server";
+import { cookies } from "next/headers";
 
 export const action = async () => {
   try {
+    console.log((await cookies()).getAll());
     return "ok";
   } catch {
     return "ng";
