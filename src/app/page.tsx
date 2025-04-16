83c1685f52e082b316ecccc378098c53a2361c09
ykarmr <yuki.arimura0508@gmail.com>
2025-04-16 11:31:56 +0900

    refactor: リクエストメソッドをPOSTからGETに変更し、ボディをコメントアウト

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,14 +8,14 @@ export default function Home() {
 
   const onclick = async () => {
     const res = await request("/posts", {
-      method: "POST",
-      body: JSON.stringify({
-        userId: 1,
-        id: 1,
-        title:
-          "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
-        body: "quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto",
-      }),
+      method: "GET",
+      // body: JSON.stringify({
+      //   userId: 1,
+      //   id: 1,
+      //   title:
+      //     "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
+      //   body: "quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto",
+      // }),
     });
 
     if (res.isOk) {
